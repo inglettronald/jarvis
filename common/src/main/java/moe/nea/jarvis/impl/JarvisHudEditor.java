8115707b7aa981a742ec6ef8da9b2ac4eeeda094
nea <nea@nea.moe>
2023-07-16 19:10:00 +0200

    Remove license headers for now

--- common/src/main/java/moe/nea/jarvis/impl/JarvisHudEditor.java
+++ common/src/main/java/moe/nea/jarvis/impl/JarvisHudEditor.java
@@ -1,18 +1,3 @@
-/*
- * This file is part of Jarvis Common Config Index (Jarvis).
- *
- * Copyright (C) 2023 Linnea Gräf
- *
- * Jarvis is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * Jarvis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with Jarvis.
- * If not, see <https://www.gnu.org/licenses/>.
- */
-
 package moe.nea.jarvis.impl;
 
 import moe.nea.jarvis.api.JarvisAnchor;
